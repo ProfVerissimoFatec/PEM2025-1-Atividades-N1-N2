eb212344bb26641699d7d0be5ffb10d291956795
Professor Verissimo <carlos.pereira70@fatec.sp.gov.br>
2025-02-21 17:45:10 -0300

    atividade n1-1

---
+++ PEM2025-1-Atividades-N1/PEM-Atividade-N1-1/codigo1.c
@@ -0,0 +1,25 @@
+/** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*
+ * Exemplo d e d e c l a r a o de Vetor *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+ #include <stdio.h>
+ #include <stdlib.h>
+ void main () {
+ int vetor [] = {1 ,2 ,3 ,4 ,5, 6,8}; // d e c l a r a o do vetor
+
+ int n2;
+ printf (" sizeof1 : %d\n", sizeof ( vetor ) ) ;
+ printf (" sizeof2 : %d\n", sizeof (int ) ) ;
+ n2 = sizeof ( vetor ) / sizeof (int ) ;
+
+
+ printf (" TAMANHO = %d\n\n",n2) ;
+ printf (" ======================\ n") ;
+ int acumulador =0;
+ for (int i=0;i<n2;i++) {
+ acumulador = vetor [i]+ acumulador ;
+ printf (" Contador parcial ... %d\n",acumulador )
+;
+ }
+ printf (" ======================\ n") ;
+ printf (" Soma total : %d\n",acumulador ) ;
+ }
